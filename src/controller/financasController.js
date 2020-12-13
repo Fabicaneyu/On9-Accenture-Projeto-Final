005abceb42cd7f27ef31979f23937e4cfa88fa82
Fabi <fabicaneyu@gmail.com>
2020-12-13 00:23:58 -0300

    realizada primeira rota POST na API

--- src/controller/financasController.js
+++ src/controller/financasController.js
@@ -15,6 +15,23 @@ const getAll = (request, response) => {
     })
 }
 
+const addFinancas = (request, response) => {
+    const financasDoBody = request.body
+    const financas = new financasCollections(financasDoBody)
+
+    financas.save((error) => {
+        if(error){
+            return response.status(400).send(error)
+        } else {
+            return response.status(200).send({
+                mensagem:"POST com sucesso",
+                financas
+            })  
+        }
+    })
+}
+
 module.exports = {
-    getAll
+    getAll,
+    addFinancas
 }
--- src/routes/financasRoute.js
+++ src/routes/financasRoute.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const controller = require("../controller/financasController")
 
 router.get("/", controller.getAll)
+router.post("/criar", controller.addFinancas)
 
 module.exports = router
